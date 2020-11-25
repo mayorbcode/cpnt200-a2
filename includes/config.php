a671ed37d648f1710aadc88edd19a5734401ae10
Patrick Bello <mayorbello@yahoo.com>
2020-11-24 23:29:45 -0700

    Still testing remote DB for heroku deployment

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,10 @@
 <?php
 
-// DB connection
-$conn = mysqli_connect('localhost', 'root', '', 'aviano-db');
+// local DB connection
+// $conn = mysqli_connect('localhost', 'root', '', 'aviano-db');
+
+// remote DB connection
+$conn = mysqli_connect('r1bsyfx4gbowdsis.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'utr9fhy2j1401ypy', 'qo4pme8782cgrnid', 'zkry08kspo7qixa4');
 
 // check connection
 if(!$conn){
